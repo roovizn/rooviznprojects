44309fafd0e254771e17cd7da23f02a64faeef36
roovizn <110783594+roovizn@users.noreply.github.com>
2022-12-12 02:45:58 +0600

    first commit

---
+++ New project/js/script.js
@@ -0,0 +1,8 @@
+const element = document.querySelector('.select');
+const choices = new Choices(element, {
+	searchEnabled: false,
+	itemSelectText: '',
+	shouldSort: false,
+	position: 'bottom'
+});
+
